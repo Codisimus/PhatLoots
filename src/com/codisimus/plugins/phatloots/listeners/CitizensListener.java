4054ed36dfe437f35f4eb01125049f08c504910a
Cody Sommer <codisimus@gmail.com>
2014-01-03 13:08:49 -0500

    Fixed Citizens support, Added support for NPCs to spawn with equipment

--- src/com/codisimus/plugins/phatloots/listeners/CitizensListener.java
+++ src/com/codisimus/plugins/phatloots/listeners/CitizensListener.java
@@ -3,20 +3,37 @@ package com.codisimus.plugins.phatloots.listeners;
 import com.codisimus.plugins.phatloots.PhatLoot;
 import com.codisimus.plugins.phatloots.PhatLoots;
 import net.citizensnpcs.api.event.NPCDeathEvent;
+import net.citizensnpcs.api.event.NPCSpawnEvent;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 /**
- * Listens for NPC Deaths and checks if there is a PhatLoot named after them
+ * Listens for NPC Spawns/Deaths and checks if there is a PhatLoot named after them
  *
  * @author Cody
  */
 public class CitizensListener implements Listener {
     @EventHandler (ignoreCancelled = true)
+    public void onNPCSpawn(NPCSpawnEvent event) {
+        PhatLoot phatLoot = PhatLoots.getPhatLoot("NPC" + event.getNPC().getName() + "Spawn");
+        if (phatLoot != null) {
+            Entity entity = event.getNPC().getEntity();
+            if (entity instanceof LivingEntity) {
+                phatLoot.rollForEquipment((LivingEntity) entity, 0);
+            }
+        }
+    }
+
+    @EventHandler (ignoreCancelled = true)
     public void onNPCDeath(NPCDeathEvent event) {
         PhatLoot phatLoot = PhatLoots.getPhatLoot("NPC" + event.getNPC().getName());
         if (phatLoot != null) {
-            event.setDroppedExp(phatLoot.rollForMobDrops(null, event.getNPC().getBukkitEntity().getKiller(), event.getDrops()));
+            Entity entity = event.getNPC().getEntity();
+            if (entity instanceof LivingEntity) {
+                event.setDroppedExp(phatLoot.rollForMobDrops(null, ((LivingEntity) entity).getKiller(), event.getDrops()));
+            }
         }
     }
 }
