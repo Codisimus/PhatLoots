921ef67cb11a7f3b758b4f57047eeaeac888c9d8
cody-sommer <cody.sommer@thomsonreuters.com>
2015-03-10 13:55:14 -0400

    MERGED - PR (Finish detection method #17) by CullanP

--- src/com/codisimus/plugins/phatloots/gui/InventoryListener.java
+++ src/com/codisimus/plugins/phatloots/gui/InventoryListener.java
@@ -90,12 +90,13 @@ public class InventoryListener implements Listener {
         if (!infoViewers.containsKey(playerUUID)) {
             return;
         } else { //Fixes glitch of not detecting inventory close
-            //if (!event.getInventory().getTitle().startsWith((infoViewers.get(playerUUID).name))) {
-            //    infoViewers.remove(playerUUID).save(); //Save the PhatLoot in case it has been modified
-            //    pageStacks.get(playerUUID).empty();
-            //    pageStacks.remove(playerUUID);
-            //    return;
-            //}
+            if (!event.getInventory().getTitle().contains("Loot")
+                    && !event.getInventory().getTitle().contains("Collection")) {
+                infoViewers.remove(playerUUID).save(); //Save the PhatLoot in case it has been modified
+                pageStacks.get(playerUUID).empty();
+                pageStacks.remove(playerUUID);
+                return;
+            }
         }
 
         //Don't allow any inventory clicking
